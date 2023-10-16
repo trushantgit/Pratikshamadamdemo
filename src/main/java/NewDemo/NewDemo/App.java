742e6cdaaf0dc96ab58af40ca68546f60da10cf8
trushant <trushantsolat29@gmail.com>
2023-10-16 19:38:26 +0530

    my first commit

---
+++ src/main/java/NewDemo/NewDemo/App.java
@@ -0,0 +1,13 @@
+package NewDemo.NewDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/NewDemo/NewDemo/AppTest.java
@@ -0,0 +1,20 @@
+package NewDemo.NewDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
